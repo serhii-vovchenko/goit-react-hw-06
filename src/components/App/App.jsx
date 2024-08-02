7b1c0e0e8fca3af0f0952e20c41d42b979ec9745
SergeyVovchenko <serega.vovchenko@gmail.com>
2024-08-02 17:50:21 +0200

    react-hw-06

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,82 +1,20 @@
-import { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
+import './App.css';
 
 import ContactForm from '../ContactForm/ContactForm';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
 
-import './App.css';
-
-const LOCAL_STORAGE_KEY = 'user-contacts';
-
-const defaultStateData = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
-const initialValues = {
-    name: '',
-    number: '',
-};
-
 function App() {
-    const [contacts, setContacts] = useState(() => {
-        const contactsWithLocalStorage = JSON.parse(
-            window.localStorage.getItem(LOCAL_STORAGE_KEY)
-        );
-
-        if (contactsWithLocalStorage) {
-            return contactsWithLocalStorage;
-        }
-
-        return defaultStateData;
-    });
-
-    const [filter, setFilter] = useState('');
-
-    useEffect(() => {
-        window.localStorage.setItem(
-            LOCAL_STORAGE_KEY,
-            JSON.stringify(contacts)
-        );
-    }, [contacts]);
-
-    const onFilterContacts = contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    const addNewContact = data => {
-        const newContactAddId = { ...data, id: nanoid() };
-
-        setContacts(prev => {
-            return [...prev, newContactAddId];
-        });
-    };
-
-    const deleteContact = deleteId => {
-        setContacts(prev => {
-            return prev.filter(contact => contact.id !== deleteId);
-        });
-    };
-
-    return (
-        <div className="appWrapper">
-            <div className="formContainer">
-                <h1 className="title">Phonebook</h1>
-                <ContactForm
-                    addNewContact={addNewContact}
-                    initialValues={initialValues}
-                />
-                <SearchBox value={filter} onFilter={setFilter} />
-            </div>
-            <ContactList
-                data={onFilterContacts}
-                deleteContact={deleteContact}
-            />
-        </div>
-    );
+  return (
+    <div className="appWrapper">
+      <div className="formContainer">
+        <h1 className="title">Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+      </div>
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,24 @@
 import s from './ContactList.module.css';
 import Contact from '../Contact/Contact';
+import { useSelector } from 'react-redux';
+import { selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
 
-const ContactList = ({ data, deleteContact }) => {
-    return (
-        <ul className={s.wrapper}>
-            {data.map(contact => {
-                return (
-                    <Contact
-                        key={contact.id}
-                        data={contact}
-                        deleteContact={deleteContact}
-                    />
-                );
-            })}
-        </ul>
-    );
+const ContactList = () => {
+  const allContacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const showFilteredContacts = allContacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={s.wrapper}>
+      {showFilteredContacts.map(item => {
+        return <Contact key={item.id} data={item} />;
+      })}
+    </ul>
+  );
 };
 
 export default ContactList;
